4159fb170128075da4906d1516948dde70ccc17f
Doris Morales <moralesdoris880@gmail.com>
2022-03-09 20:26:01 -0500

    Navbar added and css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import {Route, Routes } from "react-router-dom";
 import Home from './Home';
 import About from "./About";
@@ -6,10 +6,34 @@ import Projects from "./Projects";
 import './App.css';
 
 function App() {
+  const[burgerking,setBurgerKing]=useState("menu");
+  function handleBurger(e){
+    e.preventDefault();
+    if(burgerking==="menu"){
+    setBurgerKing("close")
+    }
+    else{
+    setBurgerKing("menu")
+    }
+
+}
   return (
     <div className="App">
+      <div>
+          <div id="navbar">
+            <h3>DORIS MORALES</h3>
+            <i class="material-icons" id="burger" onClick={handleBurger}>{burgerking}</i>
+          </div>
+                <hr />
+      </div>
+      {burgerking==="close"?
+            <ul id="nav" style={{position:"absolute"}}>
+                <a href="/about">About</a>
+                <a href="/projects">Projects</a>
+                <a href="/about">Contact</a>
+            </ul>:null }
       <Routes>
-      <Route path="/" exact element={<Home />}>
+      <Route path="/" exact element={<Home burgerking={burgerking}/>}>
       </Route>
       <Route path="/about" exact element={<About />}>
       </Route>
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React,{useState} from "react"; 
 
-function Home(){
+function Home({burgerking}){
+    const[display,setDisplay]=useState("none");
+    
     return(
         <div>
-            <div id="navbar">
-                <p>Doris Morales</p>
-                <i class="material-icons" id="burger">menu</i>
-            </div>
             <div id="top">
                 <img id="pfp" src="https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/275614533_379443737334355_186047330017330400_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=SEJUZ2u6gE4AX_O6qJl&_nc_ht=scontent-lga3-1.xx&oh=00_AT_aRn4zfnn3QyZMwpf23s2Z5VGnLx-K-Io4KYCUqNysWg&oe=622ED795" alt="Doris"></img>
-                <div>
+                <div className="postit">
                 <h1>Hi, I'm Doris</h1>
-                <p>I'm a Software Engineer from New York.</p>
+                <h2>I'm a Software Engineer<br/> from New York.</h2>
                 <a href="/About">About Me</a>
                 </div>
             </div>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Green_Arrow_Up.svg/1200px-Green_Arrow_Up.svg.png" href="#down"></img>
+            <div id="middle">
+            <a href="#down"><img id="arrow" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Green_Arrow_Up.svg/1200px-Green_Arrow_Up.svg.png" ></img></a>
+            </div>
             <div id="down">
                 <h1>Projects</h1>
                 <div>
